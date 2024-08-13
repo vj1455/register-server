9a531fc9847016aa6ce1be6a1aac8b5df0936d60
vj1455 <pendemvijay0505@gmail.com>
2024-08-13 16:10:18 +0530

    vercel view delete

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 app.use(cors())
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
